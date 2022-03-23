aeeafb846e10afb2b49565d6022d7954f114566f
Vivek Nair <70468313+Vivek0306@users.noreply.github.com>
2022-03-23 11:40:07 +0530

    Code updates

--- lyrics_finder.py
+++ lyrics_finder.py
@@ -2,25 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
+from time import sleep
 
 print("HELLO THERE!\n Welcome to Lyrics Finder. Enter the name of the song your want to search the lyrics for.")
 song_name = input("Song Name => ")
 
 #CHROME DRIVER PATH. Use this variable to update the Chrome driver path
-webdriver_path = Service("<<<chrome driver path goes here>>>")
+webdriver_path = Service("<<<Chrome Driver path goes here>>>")
 
 option = webdriver.ChromeOptions()
 option.add_argument('headless')
 driver = webdriver.Chrome(service = webdriver_path, options = option)
-driver.get("https://www.google.com")
 
+print("\nSearching",end="")
+for i in range(10):
+    sleep(0.5)
+    print(".", end="")
+
+driver.get("https://www.google.com")
 search_bar = driver.find_element_by_class_name('gLFyf')
 search_bar.send_keys(song_name + " lyrics" + Keys.ENTER)
+
+
 try:
     lyrics = driver.find_element_by_class_name('sATSHe')
     print("\nHow do you want the lyrics to be presented\n 1. CLI\n 2. Text File")
     choice = int(input("Enter your choice => "))
-
 
     if choice == 1:
         print("\nHere is your lyrics........\n")
@@ -33,7 +40,5 @@ try:
 except NoSuchElementException:
     print("\nSong not found!!")
     pass
-
-
 
 print("\n\nTHANK YOU FOR USING LYRIC FINDER APP!\n made with <3 by Vivek :)")
